cc6169bc06073e5e28c4736dad2f2c6f09e51358
joy <joy@joydeMacBook-Pro.local>
2017-08-27 21:41:41 +0800

    update demo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
-index.js
+import Vue from 'vue'
+import VueMessageMask from '../dist/VueMessageMask.js'
+
+Vue.use(VueMessageMask)
+
+new Vue({
+	el:'#app',
+	data:{
+		message:'hello world'
+	},
+	methods:{
+            showloading:function(vm){
+            	console.log(vm)
+            	this.message = '123'
+            console.log(window.test = this)
+                this.VMaskController.showVMaskLoading({loadingText:this.message})
+            },
+            showmsg(){
+                this.VMaskController.showVMaskMultiMsg({
+                    message:this.message,
+                    showOkButton:true,
+                    okText:'知道了',
+                    okAction:()=>{
+                        console.log('点击了确认按钮')
+                    }
+                })
+            },
+            showconfirm(){
+                this.VMaskController.showVMaskMultiMsg({
+                    message:this.message,
+                    showOkButton:true,
+                    showCancelButton:true,
+                    okText:'知道了',
+                    cancelText:'取消',
+                    okAction:()=>{
+                        console.log('点击了确认按钮')
+                    },
+                    cancelAction:()=>{
+                        console.log('点击了取消按钮')
+                    }
+                })
+            },
+            showdiy(){
+                const _html_ = '<div style="height:100px;width:200px;background-color:#fff;font-size:20px">测试自定义</div>'
+                this.VMaskController.showDiyVMask(_html_)
+            }
+        }
+})
